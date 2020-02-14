6555ce2e03ab8940db74230d703beaf092811d42
Jake Feigum <jfig203@gmail.com>
2020-02-14 18:38:34 +0000

    Updated calculator and hello.rb

--- Lesson_2/calculator.rb
+++ Lesson_2/calculator.rb
@@ -7,28 +7,42 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer) 
 
-Kernel.puts("Welcome to Calculator!")
 
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-  
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-else 
-  result = number1.to_f() / number2.to_f()
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts(result)
+
+prompt("Welcome to Calculator!")
+
+loop do
+  prompt("What's the first number?")
+  number1 = Kernel.gets().chomp()
+  
+  if valid_number?(number1)
+    break
+  else
+    prompt("Hmm... that doesn't look like a valid number.")
+end
+prompt("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+
+result = 
+  case operator
+  when '1'
+    number1.to_i() + number2.to_i()
+  when '2'
+    number1.to_i() - number2.to_i()
+  when '3'
+    number1.to_i() * number2.to_i()
+  when '4'
+    number1.to_f() / number2.to_f()
+  end
+prompt("The result is #{result}.")
 
 
   
--- Lesson_2/hello.rb
+++ Lesson_2/hello.rb
@@ -1,2 +1,4 @@
 
 Kernel.puts("hello world")
+
+
