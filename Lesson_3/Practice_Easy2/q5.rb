0b720ec5826adbac152c737de8f843854c4db09e
Jake Feigum <jfig203@gmail.com>
2020-02-19 23:19:19 -0600

    new commit

---
+++ Lesson_3/Practice_Easy2/q5.rb
@@ -0,0 +1,12 @@
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones
+
+flintstones << "Dino"
+
+flintstones.concat(%w(Dino Hoppy))
+
+flintstones.push("Dino").push("Hoppy")
+
+p flintstones
